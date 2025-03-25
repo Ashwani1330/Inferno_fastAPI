2659627fb266293a30a3fbbc55029704bb67539b
Ashwani1330 <ashwanihabri@gmail.com>
2025-03-26 03:07:20 +0530

    feat: fixing ports

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
-import uvicorn
 import os
+import uvicorn
 
 if __name__ == "__main__":
-    # Get port from environment variable or default to 8000
-    port = int(os.environ.get("PORT", 8000))
-    
-    # Run the application
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
